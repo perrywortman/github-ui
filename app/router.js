4846ecce2a4c9713cc01ce9ea8808b0c4f7f8b77
W. Perry Wortman <pwortman@visioncreativegroup>
2016-03-08 10:26:10 -0500

    Initial commit

--- app/router.js
+++ app/router.js
@@ -6,6 +6,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('orgs'); // orgs
+  this.route('org', { path: 'org/:id'}, function() { // org/netflix
+    this.route('repos'); // org/netflix/repos
+    this.route('repo', { path: ':repoid'}, function() { // org/netflix/hystrix
+      this.route('contributors'); // org/netflix/hystrix/contributors
+      this.route('issues'); // org/netflix/hystrix/issues
+    });
+  });
+  this.route('not-found', { path: '*path' });
 });
 
 export default Router;
---
+++ tests/unit/routes/org/netflix-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:org/netflix', 'Unit | Route | org/netflix', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
